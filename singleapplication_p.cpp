3da53a552b07f12be7efa78dc49525bfde611be0
Jonas Kvinge <jonas@jkvinge.net>
2019-01-12 02:19:22 +0100

    Add cmake files

--- singleapplication_p.cpp
+++ singleapplication_p.cpp
@@ -42,7 +42,6 @@
 #include <QtNetwork/QLocalServer>
 #include <QtNetwork/QLocalSocket>
 
-#include "singleapplication.h"
 #include "singleapplication_p.h"
 
 #ifdef Q_OS_WIN
@@ -50,7 +49,7 @@
     #include <lmcons.h>
 #endif
 
-SingleApplicationPrivate::SingleApplicationPrivate( SingleApplication *q_ptr )
+SingleApplicationPrivate::SingleApplicationPrivate( SINGLEAPPLICATION_CLASS *q_ptr )
     : q_ptr( q_ptr )
 {
     server = nullptr;
@@ -81,25 +80,25 @@ SingleApplicationPrivate::~SingleApplicationPrivate()
 void SingleApplicationPrivate::genBlockServerName()
 {
     QCryptographicHash appData( QCryptographicHash::Sha256 );
-    appData.addData( "SingleApplication", 17 );
-    appData.addData( SingleApplication::app_t::applicationName().toUtf8() );
-    appData.addData( SingleApplication::app_t::organizationName().toUtf8() );
-    appData.addData( SingleApplication::app_t::organizationDomain().toUtf8() );
+    appData.addData( QT_STRINGIFY(SINGLEAPPLICATION_CLASS), sizeof(QT_STRINGIFY(SINGLEAPPLICATION_CLASS)) );
+    appData.addData( SINGLEAPPLICATION_CLASS::app_t::applicationName().toUtf8() );
+    appData.addData( SINGLEAPPLICATION_CLASS::app_t::organizationName().toUtf8() );
+    appData.addData( SINGLEAPPLICATION_CLASS::app_t::organizationDomain().toUtf8() );
 
-    if( ! (options & SingleApplication::Mode::ExcludeAppVersion) ) {
-        appData.addData( SingleApplication::app_t::applicationVersion().toUtf8() );
+    if( ! (options & SINGLEAPPLICATION_CLASS::Mode::ExcludeAppVersion) ) {
+        appData.addData( SINGLEAPPLICATION_CLASS::app_t::applicationVersion().toUtf8() );
     }
 
-    if( ! (options & SingleApplication::Mode::ExcludeAppPath) ) {
+    if( ! (options & SINGLEAPPLICATION_CLASS::Mode::ExcludeAppPath) ) {
 #ifdef Q_OS_WIN
-        appData.addData( SingleApplication::app_t::applicationFilePath().toLower().toUtf8() );
+        appData.addData( SINGLEAPPLICATION_CLASS::app_t::applicationFilePath().toLower().toUtf8() );
 #else
-        appData.addData( SingleApplication::app_t::applicationFilePath().toUtf8() );
+        appData.addData( SINGLEAPPLICATION_CLASS::app_t::applicationFilePath().toUtf8() );
 #endif
     }
 
     // User level block requires a user specific data in the hash
-    if( options & SingleApplication::Mode::User ) {
+    if( options & SINGLEAPPLICATION_CLASS::Mode::User ) {
 #ifdef Q_OS_WIN
         wchar_t username [ UNLEN + 1 ];
         // Specifies size of the buffer on input
@@ -142,7 +141,7 @@ void SingleApplicationPrivate::initializeMemoryBlock()
 
 void SingleApplicationPrivate::startPrimary()
 {
-    Q_Q(SingleApplication);
+    Q_Q(SINGLEAPPLICATION_CLASS);
 
     // Successful creation means that no main process exists
     // So we start a QLocalServer to listen for connections
@@ -151,7 +150,7 @@ void SingleApplicationPrivate::startPrimary()
 
     // Restrict access to the socket according to the
     // SingleApplication::Mode::User flag on User level or no restrictions
-    if( options & SingleApplication::Mode::User ) {
+    if( options & SINGLEAPPLICATION_CLASS::Mode::User ) {
       server->setSocketOptions( QLocalServer::UserAccessOption );
     } else {
       server->setSocketOptions( QLocalServer::WorldAccessOption );
@@ -326,7 +325,7 @@ void SingleApplicationPrivate::readInitMessageHeader( QLocalSocket *sock )
 
 void SingleApplicationPrivate::readInitMessageBody( QLocalSocket *sock )
 {
-    Q_Q(SingleApplication);
+    Q_Q(SINGLEAPPLICATION_CLASS);
 
     if (!connectionMap.contains( sock )) {
         return;
@@ -379,7 +378,7 @@ void SingleApplicationPrivate::readInitMessageBody( QLocalSocket *sock )
 
     if( connectionType == NewInstance ||
         ( connectionType == SecondaryInstance &&
-          options & SingleApplication::Mode::SecondaryNotification ) )
+          options & SINGLEAPPLICATION_CLASS::Mode::SecondaryNotification ) )
     {
         Q_EMIT q->instanceStarted();
     }
@@ -391,7 +390,7 @@ void SingleApplicationPrivate::readInitMessageBody( QLocalSocket *sock )
 
 void SingleApplicationPrivate::slotDataAvailable( QLocalSocket *dataSocket, quint32 instanceId )
 {
-    Q_Q(SingleApplication);
+    Q_Q(SINGLEAPPLICATION_CLASS);
     Q_EMIT q->receivedMessage( instanceId, dataSocket->readAll() );
 }
 
