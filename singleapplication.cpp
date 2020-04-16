5f4f2e0a2532d793ba3f876f87cfaca0f7725607
Emeric <emeric.grange@gmail.com>
2020-04-16 19:00:34 +0200

    qWarning is unnecessary for a transparent fallback

--- singleapplication.cpp
+++ singleapplication.cpp
@@ -48,7 +48,7 @@ SingleApplication::SingleApplication( int &argc, char *argv[], bool allowSeconda
 #if defined(Q_OS_ANDROID) || defined(Q_OS_IOS)
     // On Android and iOS since the library is not supported fallback to
     // standard QApplication behaviour by simply returning at this point.
-    qWarning() << "SingleApplication is not supported on Android and iOS systems.";
+    qDebug() << "SingleApplication is not supported on Android and iOS systems.";
     return;
 #endif
 
